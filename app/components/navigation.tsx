f06fb8e0dab46e1e4921e25bf50731358df1cd61
akmmubashir <akmmubashir@gmail.com>
2025-12-27 23:17:15 +0530

    page implemented

---
+++ app/components/navigation.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+
+type Props = {
+    type?: string
+}
+
+const Navigation = (props: Props) => {
+    const navigationData = [
+        { name: 'Home', href: '/' },
+        { name: 'Projects', href: '#projects' }, 
+        { name: 'Contact Us', href: '#contact' },
+    ];
+    return (
+        <div className='flex gap-5 uppercase max-xl:hidden'>{navigationData.map((item) => (
+            <Link key={item.name} href={item.href} className={`text-[16px] ${props.type === "transparent" ? "text-white hover:text-[#244952]" : "text-[#160A0A] hover:text-[#244952]"} ${props.type === "scroll" ? "font-medium" : ""}`}>{item.name}</Link>
+        ))}</div>
+    )
+}
+
+export default Navigation
